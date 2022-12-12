31eac2637ee691b795adfce51df25a1fe01a8dfc
mrdinghr <mr_dinghr@163.com>
2022-12-12 18:27:49 +0100

    build up simple PD Controller

--- script/control.py
+++ script/control.py
@@ -1,1 +1,24 @@
 import torch as torch
+import numpy as np
+
+
+class Controller:
+    def __init__(self):
+        self.kp = 5
+        self.kv = 1
+        self.pos = None
+        self.vel = None
+
+    def reset(self, mallet_pos, mallet_vel, kp=None, kv=None):
+        self.pos = mallet_pos
+        self.vel = mallet_vel
+        if kp is not None:
+            self.kp = kp
+        if kv is not None:
+            self.kv = kv
+
+# calculate action u for each time step
+# current just simple pd controller
+    def action(self, puck_pos, puck_vel):
+        u = self.kp * (puck_pos - self.pos) + self.kv * (puck_vel - self.vel)
+        return np.append(u, np.zeros(4))
--- script/main.py
+++ script/main.py
@@ -1,8 +1,49 @@
 import mujoco
 import os
+from control import Controller
+from mushroom_rl.utils.mujoco.viewer import MujocoGlfwViewer
+import numpy as np
+import time
 
-table_dir = os.path.join("table_strike.xml")
-mj_model = mujoco.MjModel.from_xml_path(table_dir)
+
+def main():
+    table_dir = os.path.join("../air_hockey_table/table_test.xml")
+    mj_model = mujoco.MjModel.from_xml_path(table_dir)
+    mj_model.opt.timestep = 0.001
+    mj_data = mujoco.MjData(mj_model)
+    mj_viewer = MujocoGlfwViewer(mj_model, 0.001, start_paused=True)
+    controller = Controller()
+    trajectory_num = 10
+    for _ in range(trajectory_num):
+
+        # Set up Puck (Red) Initial State
+        mj_data.qpos[0:3] = np.random.rand(3)
+        mj_data.qvel[:] = 0.
+        mj_data.qvel[0:3] = np.random.rand(3)
+
+        # set up Mallet
+        mj_data.qpos[-3] = -0.3
+        mj_data.qpos[-2] = 0
+
+        t = 0.
+        mujoco.mj_step(mj_model, mj_data, 1)
+        mj_viewer.render(mj_data)
+        while t < 2:
+            controller.reset(mj_data.qpos[-3:-1], mj_data.qvel[-3:-1])
+            u = controller.action(mj_data.qpos[0:2], mj_data.qvel[0:2])
+            mj_data.qvel[-3] = u[0]
+            mj_data.qvel[-2] = u[1]
+            mujoco.mj_step(mj_model, mj_data, 1)
+            mj_viewer.render(mj_data)
+            if mj_model.geom('puck').id in mj_data.contact.geom1 or mj_model.geom('puck').id in mj_data.contact.geom1:
+                print("contact")
+            t += 0.001
+            time.sleep(1 / 1000)
+
+
+
+if __name__ == '__main__':
+    main()
 
 
 
